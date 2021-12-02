4cbf1a9768cd0f7c371abe875c645a6b9eaf80b5
mjarkk <mkopenga@gmail.com>
2021-12-02 12:59:17 +0100

    switch map resolution while zooming

--- components/map.tsx
+++ components/map.tsx
@@ -18,61 +18,65 @@ interface MapT {
     lineGroups: { [key: string]: Group }
 }
 
+type groupOfLines = Array<Array<
+    [number, number, number, number] // point: [x1, y1, x2, y2]
+>>
+
 function p5Body(s: p5) {
     let pointsToDraw = [] as Array< // groups
         {
-            drawFromTil: [number, number],
-            lines: Array< // group lines
-                Array< // line points
-                    [number, number, number, number] // point: [x1, y1, x2, y2]
-                >
-            >
+            drawFromTil: [number, number, number] // [from, resolutionSwitchPoint, til]
+            linesHighRes: groupOfLines
+            linesLowRes: groupOfLines
         }
     >
     let transformX = 0
     let transformY = 0
 
     const setPointsToDraw = () => {
-        const drawFromTilMap = {
-            europe_countries: [0, 20],
-            dutch_provinces: [10, 100],
-            groningen_city: [30, 100],
+        const drawFromTilMap: { [key: string]: [number, number, number] } = {
+            europe_countries: [0, 4, 20],
+            dutch_provinces: [10, 40, 100],
+            groningen_city: [30, 50, 100],
         }
-        const diffMap = {
-            europe_countries: .04,
-            dutch_provinces: .02,
-            groningen_city: .004,
+        const diffMap: { [key: string]: [number, number] } = { // high diff, low diff
+            europe_countries: [.02, .08],
+            dutch_provinces: [.01, .02],
+            groningen_city: [.002, .004],
         }
 
         const mapToScreenRatio = s.height / map.height
         pointsToDraw = Object.entries(map.lineGroups).map(([key, group]) => {
             const maxDiff = diffMap[key]
 
-            const lines = group.lines.map(points => {
-                const res = [] as Array<[number, number, number, number]>
-                let lastUsedIdx = 0
-                for (let idx = 1; idx < points.length; idx++) {
-                    const [x1, y1] = points[lastUsedIdx]
-                    const [x2, y2] = points[idx]
+            const [linesHighRes, linesLowRes] = maxDiff.map(maxDiff =>
+                group.lines.map(points => {
+                    const res = [] as Array<[number, number, number, number]>
+                    let lastUsedIdx = 0
+                    for (let idx = 1; idx < points.length; idx++) {
+                        const [x1, y1] = points[lastUsedIdx]
+                        const [x2, y2] = points[idx]
 
-                    const [xDiff, yDiff] = [x1 - x2, y1 - y2]
-                    if (idx != points.length - 1 && ((xDiff < maxDiff && xDiff > -maxDiff) || (yDiff < maxDiff && yDiff > -maxDiff))) {
-                        continue
+                        const [xDiff, yDiff] = [x1 - x2, y1 - y2]
+                        if (idx != points.length - 1 && ((xDiff < maxDiff && xDiff > -maxDiff) || (yDiff < maxDiff && yDiff > -maxDiff))) {
+                            continue
+                        }
+                        lastUsedIdx = idx
+
+                        res.push([
+                            x1 * mapToScreenRatio,
+                            y1 * mapToScreenRatio,
+                            x2 * mapToScreenRatio,
+                            y2 * mapToScreenRatio,
+                        ])
                     }
-                    lastUsedIdx = idx
-
-                    res.push([
-                        x1 * mapToScreenRatio,
-                        y1 * mapToScreenRatio,
-                        x2 * mapToScreenRatio,
-                        y2 * mapToScreenRatio,
-                    ])
-                }
-                return res
-            })
+                    return res
+                })
+            )
 
             return {
-                lines,
+                linesHighRes,
+                linesLowRes,
                 drawFromTil: drawFromTilMap[key],
             }
         })
@@ -107,7 +111,7 @@ function p5Body(s: p5) {
         s.stroke(255)
         s.strokeWeight(3)
 
-        const time = 10_000
+        const time = 20_000 // 20 seconds
         const zoomLvl = (performance.now() - zoomTimer) % time
         let zoom = 1
         if (zoomLvl > (time / 2)) {
@@ -127,12 +131,12 @@ function p5Body(s: p5) {
         // s.rect(transformX - 5 + nonZoomOffsetX, transformY - 5 + nonZoomOffsetY, 10, 10)
 
         for (const group of pointsToDraw) {
-            const [drawFrom, drawTil] = group.drawFromTil
+            const [drawFrom, resolutionSwitch, drawTil] = group.drawFromTil
             if (drawFrom > zoom || drawTil < zoom) {
                 continue
             }
 
-            for (const lines of group.lines) {
+            for (const lines of zoom > resolutionSwitch ? group.linesHighRes : group.linesLowRes) {
                 for (const line of lines) {
                     const [x1, y1, x2, y2] = line.map((p, idx) => p * zoom + (idx % 2 === 0 ? offsetX : offsetY))
 
