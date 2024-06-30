695f92faa01f9ad104ee5b72e7af1a42a47b84b1
mjarkk <mkopenga@gmail.com>
2024-06-30 18:21:48 +0200

    Updated deps and more things

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,30 @@
-import './globals.css'
-import type { Metadata } from 'next'
+import "./globals.css";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'Mark Kopenga\'s website',
-}
+  title: "Mark Kopenga's website",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Recursive:slnt,wght,CASL,CRSV,MONO@-15..0,300..800,0..1,0..1,0..1&display=swap" rel="stylesheet" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Recursive:slnt,wght,CASL,CRSV,MONO@-15..0,300..800,0..1,0..1,0..1&display=swap"
+          rel="stylesheet"
+        />
       </head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
